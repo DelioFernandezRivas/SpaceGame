28da14cee31681fe3c09aaadea072a0f26c19502
Prueva2653 <erlendgreind@gmail.com>
2017-10-28 16:28:05 +0200

    First Commit

---
+++ Assets/Script/RamdomRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RamdomRotator : MonoBehaviour {
+	private Rigidbody rb;
+	public float tunble;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere* tunble;
+
+
+
+	}
+
+}
